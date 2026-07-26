0870ef1d2d3770349a5f21c9cc04d72bf379e3a4
vantheman <vantheman@420blaze.it>
2026-07-26 17:02:52 -0400

    Changed POSIX print behavior to use stdout

--- Indev/libs/posix/std/io/print.zig
+++ Indev/libs/posix/std/io/print.zig
@@ -4,5 +4,12 @@ const vm = @import("state");
 pub fn run() !void {
     const indirect = vm.data.get("ARG1") orelse return;
     const msg = vm.data.get(indirect) orelse return;
-    std.debug.print("{s}\n", .{msg});
+
+    var buf: [1024]u8 = undefined;
+    var writer = std.fs.File.stdout().writer(&buf);
+    const stdout = &writer.interface;
+
+    try stdout.print("{s}\n", .{msg});
+    try stdout.flush();
 }
+
--- Indev/libs/posix/std/io/printLn.zig
+++ Indev/libs/posix/std/io/printLn.zig
@@ -4,5 +4,12 @@ const vm = @import("state");
 pub fn run() !void {
     const indirect = vm.data.get("ARG1") orelse return;
     const msg = vm.data.get(indirect) orelse return;
-    std.debug.print("{s}", .{msg});
+
+    var buf: [1024]u8 = undefined;
+    var writer = std.fs.File.stdout().writer(&buf);
+    const stdout = &writer.interface;
+
+    try stdout.print("{s}", .{msg});
+    try stdout.flush();
 }
+
