4013b58aaf5b7fe66c59cc7f6354ed8be50b5d46
vantheman <vantheman@420blaze.it>
2026-07-28 14:41:00 -0400

    more term work

---
+++ Indev/libs/posix/std/term/screen/clearToEnd.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+const vm = @import("state");
+
+pub fn run() !void {
+    var buf: [1024]u8 = undefined;
+    var writer = std.fs.File.stdout().writer(&buf);
+    const stdout = &writer.interface;
+
+    try stdout.print("\x1b[J", .{});
+    try stdout.flush();
+}
--- Indev/libs/posix/std/term/screen/libs.zig
+++ Indev/libs/posix/std/term/screen/libs.zig
@@ -1,1 +1,4 @@
 pub const clear = @import("clear.zig");
+pub const clearToEnd = @import("clearToEnd.zig");
+pub const clearToEndOfLine = @import("clearToEndOfLine.zig");
+pub const clearLine = @import("clearLine.zig");
