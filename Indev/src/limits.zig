f834dfb9be920864b2bdd23eaa391f9e4753b17a
vantheman <vantheman@420blaze.it>
2026-07-29 00:52:45 -0600

    limits

--- Indev/src/limits.zig
+++ Indev/src/limits.zig
@@ -1,22 +1,22 @@
-pub const inst_max: u32 = 100_000_000;
+pub const inst_max: u32 = 4294967295;
 pub var inst_curr: u32 = 0;
 
-pub const inst_new_max: u32 = 1_000_000;
+pub const inst_new_max: u32 = 4294967295;
 pub var inst_new_curr: u32 = 0;
 
-pub const inst_escape_max: u32 = 1_000;
+pub const inst_escape_max: u32 = 4294967295;
 pub var inst_escape_curr: u32 = 0;
 
-pub const inst_func_max: u32 = 1_00;
+pub const inst_func_max: u32 = 4294967295;
 pub var inst_func_curr: u32 = 0;
 
-pub const inst_call_max: u32 = 10_000;
+pub const inst_call_max: u32 = 4294967295;
 pub var inst_call_curr: u32 = 0;
 
-pub const inst_if_max: u32 = 10_000;
+pub const inst_if_max: u32 = 4294967295;
 pub var inst_if_curr: u32 = 0;
 
-pub const inst_return_max: u32 = 15_000;
+pub const inst_return_max: u32 = 4294967295;
 pub var inst_return_curr: u32 = 0;
 
 // change those to something sane at release
