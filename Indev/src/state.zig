4f831d404d19f3fd4283af732aaaf852b78d1040
vantheman <vantheman@420blaze.it>
2026-08-18 04:51:12 +0000

    patch

--- Indev/src/state.zig
+++ Indev/src/state.zig
@@ -19,11 +19,11 @@ pub const shword = if (build.native)
 
 pub const quword = if (build.native)
     if (@bitSizeOf(usize) == 64) u16
-    else u8 else u8 // quarter word
+    else u8 else u8; // quarter word
 
 pub const squword = if (build.native)
     if (@bitSizeOf(isize) == 64) i16
-    else i8 else i8 // signed quarter word
+    else i8 else i8; // signed quarter word
 
 pub const dword = if (build.double) u64; // double word
 pub const sdword = if (build.double) i64; // signed double word
