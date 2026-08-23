75c3ac99eac4a1af0b76edc311a72e0430db2636
vantheman <vantheman@420blaze.it>
2026-08-23 12:46:11 +0000

    work on re-write

---
+++ rewrite/src/vm/test.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../allocator/allocator.h"
+
+#include "../state/variables.h"
+#include "../state/functions.h"
+#include "../state/values.h"
+
+#include "../platform/use/print.h"
+#include "../platform/use/getstrlen.h"
+#include "../platform/use/copystr.h"
+
+void testAllocator() {
+	struct Arena *arena = arena = initAlloc(4096);
+
+	char *arenamsg1 = "Arena allocation working (1)\n";
+	char *arenamsg2 = "Arena allocation working (2)\n";
+
+	char *arenaarea1 = alloc(arena, getstrlen(arenamsg1));
+	char *arenaarea2 = alloc(arena, getstrlen(arenamsg2));
+
+	copystr(arenamsg1, arenaarea1);
+	copystr(arenamsg2, arenaarea2);
+
+	print(arenaarea1);
+	print(arenaarea2);
+
+	free(arena);
+}
+
+void testVariables() {
+	struct Arena *arena = arena = initAlloc(4096);
+	Map vars = initVars(32);
+	
+	char *varsmsg1 = "Variable tables working (1)\n";
+	char *varsmsg2 = "Variable tables working (2)\n";
+
+	char *varsarea1 = alloc(arena, getstrlen(varsmsg1));
+	char *varsarea2 = alloc(arena, getstrlen(varsmsg2));
+
+	copystr(varsmsg1, varsarea1);
+	copystr(varsmsg2, varsarea2);
+
+	putVar(&vars, "one", varsarea1);
+	putVar(&vars, "two", varsarea2);
+
+	char *varresult1 = getVar(&vars, "one");
+	char *varresult2 = getVar(&vars, "two");
+
+	print(varresult1);
+	print(varresult2);
+
+	free(arena);
+}
+
+int main() {
+	testAllocator();
+	testVariables();
+
+	return 0;
+}
