8a8b6ac84fbdf34b98cc2e5dcad319158755fe9a
vantheman <vantheman@420blaze.it>
2026-08-23 14:14:34 +0000

    better build script

--- rewrite/src/build.sh
+++ rewrite/src/build.sh
@@ -1,9 +1,53 @@
 #!/bin/sh
 
-if [ "$1" == "d" ]; then
-	cc -g -o pblvm vm/main.c
-	cc -o -g test vm/test.c
+
+echo "Finding C compiler..."
+if command -v cc >/dev/null; then
+	CC="$(which cc)"
 else
-	cc -o pblvm vm/main.c
-	cc -o test vm/test.c
+	if command -v clang >/dev/null; then
+		CC="$(which clang)"
+	else
+		if command -v pcc >/dev/null; then
+			CC="$(which pcc)"
+		else
+			if command -v gcc >/dev/null; then
+				CC="$(which gcc)"
+			else
+				echo "Error: No suitable C compiler found."
+				exit 1
+			fi
+		fi
+	fi
 fi
+
+echo "C compiler found at $CC"
+
+SRCPATH="." # path to source directory
+BINPATH="bin" # path to output directory
+
+VMSRCNAME="vm/main.c" # VM source code file name
+VMTESTSRCNAME="vm/test.c" # VMtest source code file name
+
+VMSRCPATH="$SRCPATH/$VMSRCNAME"
+VMTESTSRCPATH="$SRCPATH/$VMTESTSRCNAME"
+
+VMBINNAME="pblvm" # VM output file name
+VMTESTBINNAME="vmtest" # VMtest output file name
+
+VMBINPATH="$BINPATH/$VMBINNAME"
+VMTESTBINPATH="$BINPATH/$VMTESTBINNAME"
+
+if [ "$1" == "d" ]; then
+	FLAGS="-g"
+fi
+
+mkdir -p "$BINPATH"
+
+echo "Building VM: $CC "$FLAGS" -o $VMBINPATH $VMSRCPATH"
+$CC "$FLAGS" -o $VMBINPATH $VMSRCPATH
+
+echo "Building test suite: $CC "$FLAGS" -o $VMTESTBINPATH $VMTESTSRCPATH"
+$CC "$FLAGS" -o $VMTESTBINPATH $VMTESTSRCPATH
+
+echo "Done!"
