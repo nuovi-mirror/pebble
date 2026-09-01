eceec3bac66b513a1a93e9e52669402cc584913e
vantheman <vantheman@420blaze.it>
2026-09-01 22:17:50 +0000

    new build system

--- rewrite/src/build.sh
+++ rewrite/src/build.sh
@@ -4,17 +4,19 @@ findcc() {
 	echo "Searching for a C compiler..."
 
 	if [ -n "$1" ] && command -v "$1" && [ "$1" != "N" ]; then
+		echo "Compiler given as argument three ($1). Using that."
+	else
 		if command -v cc >/dev/null; then
-			CC="$(which cc)"
+			CC="$(command -v cc)"
 		else
-			if command -v clang; then
-				CC="$(which clang)"
-			else 
-				if command -v gcc; then
-					CC="$(which gcc)"
+			if command -v clang >/dev/null; then
+				CC="$(command -v clang)"
+			else
+				if command -v gcc >/dev/null; then
+					CC="$(command -v gcc)"
 				else
-					if command -v pcc; then
-						CC="$(which pcc)"
+					if command -v pcc >/dev/null; then
+						CC="$(command -v pcc)"
 					else
 						echo "Error: No suitable C compiler found!"
 						exit 1
@@ -23,6 +25,8 @@ findcc() {
 			fi
 		fi
 	fi
+
+	echo "C compiler found at $CC"
 }
 
 getplatform() {
@@ -139,13 +143,21 @@ findanyz() {
 	echo "Searching for a static analyzer..."
 
 	if [ -n "$1" ] && command -v "$1" && [ "$1" != "N" ]; then
-		if command -v scan-build-19 >/dev/null; then
-			ANYZ="$(which scan-build-19)"
+		echo "Static analyzer selected at argument four ($1). Using that."
+		ANYZ="$(command -v $1)"
+	else
+		if command -v scan-build >/dev/null; then
+			ANYZ="$(command -v scan-build)"
 		else
-			echo "Error: No suitable static analyzer found!"
-			echo "Continuing..."
+			if command -v scan-build-19 >/dev/null; then
+				ANYZ="$(command -v scan-build-19)"
+			else
+				echo "Warning: No static analyzer found!"
+			fi
 		fi
 	fi
+
+	echo "Static analyzer found at $ANYZ"
 }
 
 
@@ -160,7 +172,13 @@ getflags() {
 		echo "Argument one should be the platform!"
 		exit 1
 	fi
+
+	echo "Build type of $1"
+	echo "Flags of: $FLAGS"
 }
+
+rm -r platform/use/ >/dev/null 2>&1
+rm vm/main.c >/dev/null 2>&1
 
 getflags $1
 getplatform $2
@@ -175,5 +193,8 @@ $ANYZ $CC $FLAGS -o bin/vm vm/main.c
 
 echo "Building test suite : $ANYZ $CC $FLAGS -o bim/test vm/test.c"
 $ANYZ $CC $FLAGS -o bin/test vm/test.c
+
+rm -rf platform/use/
+rm vm/main.c
 
 echo "Done!"
