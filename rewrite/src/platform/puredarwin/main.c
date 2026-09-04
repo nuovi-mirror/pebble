49d1b7b341435609331f9c6e06cae6c15c0582fc
vantheman <vantheman@420blaze.it>
2026-09-04 01:30:56 +0000

    hotpatch: puredarwin

---
+++ rewrite/src/platform/puredarwin/main.c
@@ -0,0 +1,33 @@
+#undef NULL
+#define NULL ((void *)0)
+
+#include <unistd.h>
+#include <stdlib.h>
+
+/* at the time of writting this, PureDarwin has
+ * a few unfinished bits, so we need to alias
+ * these to avoid linking errors. */
+
+static long _read$UNIX2003
+(int fd, void *buf, unsigned long nbyte)
+{ return read(fd, buf, nbyte); }
+
+static int _close$UNIX2003
+(int fd)
+{ return close(fd); }
+
+static double _strtod$UNIX2003
+(const char *nptr, char **endptr)
+{ return strtod(nptr, endptr); }
+
+/* and now it is just POSIX code */
+
+#include "../platform/use/entry.h"
+#include "vm.c"
+
+int main(int argc, char **argv) {
+	Args cliargs = initargs(argc, argv);
+	Stack *stack = initstack(1024, 1024);
+	return vmmain(cliargs, stack);
+	freestack(stack);
+}
