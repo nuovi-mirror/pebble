a4d4e4e32fd01d4b3a872e0b2cd79301060cd7d9
vantheman <vantheman@thenuoviorizzonticompany.org>
2026-09-11 21:35:57 +0000

    copymem optimizatoins

--- indev/platform/freestand/copymem.c
+++ indev/platform/freestand/copymem.c
@@ -1,22 +1,47 @@
 #include "copymem.h"
 
+typedef unsigned long word; /* word size */
+
+#define wordsize sizeof(word)
+#define wordsizeminus wordsize - 1
+
 void *copymem
 (const void *src, void *dst, unsigned long len)
 {
-	const unsigned long *wsrc = (const unsigned long *)src;
-	unsigned long *wdst = (unsigned long *)dst;
-	unsigned long words = len / sizeof(unsigned long);
+	const unsigned char *charsrc = src;
+	unsigned char *chardst = dst;
+	unsigned long count = 0;
 
-	while (words--)
-		*wdst++ = *wsrc;
+	/* if aliagn differs, copy bytes */
+	if (((unsigned long)charsrc ^ (unsigned long)chardst) & wordsizeminus)
+		goto copybytes;
 
-	/* remaining bytes */
-	const unsigned char *bsrc = (const unsigned char *)wsrc;
-	unsigned char *bdst = (unsigned char *)wdst;
-	unsigned long bytes = len % sizeof(unsigned long);
+	/* align */
+	count = (-((unsigned long)charsrc)) & wordsize;
 
-	while (bytes--)
-		*bdst++ = *bsrc++;
+	if (count > len)
+		count = len;
+
+	while (count--)
+	{
+		*chardst++ = *charsrc++;
+		len--;
+	}
+
+	/* words */
+	count = len / wordsize;
+
+	while (count--)
+	{
+		*(word *)chardst = *(const word *)charsrc;
+		chardst += wordsize;
+		charsrc += wordsize;
+	}
+
+	/* tail */
+copybytes:
+	while (len--)
+		*chardst++ = *charsrc++;
 
 	return dst;
 }
