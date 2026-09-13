344cf3935ac27b0c55b143a84512bf095362c7f5
vantheman <vantheman@thenuoviorizzonticompany.org>
2026-09-13 21:46:00 +0000

    ffi

--- indev/h/variables.h
+++ indev/h/variables.h
@@ -4,21 +4,11 @@
 #include "hashmap.h"
 #include "variables.h"
 
-typedef SHashMap VarMap;
-
-static unsigned long hashVar
-(VarMap *m, char *str);
-
-VarMap initVars
-(unsigned long cap);
-
-void putVar
-(VarMap *m, char *key, void *value);
-
-void *getVar
-(VarMap *m, char *key);
-
-void freeVars
-(VarMap *m);
+typedef	SHashMap VarMap;
+static unsigned long hashVar (VarMap *m, char *str);
+VarMap initVars (unsigned long cap);
+void putVar (VarMap *m, char *key, void *value);
+void *getVar (VarMap *m, char *key);
+void freeVars (VarMap *m);
 
 #endif
