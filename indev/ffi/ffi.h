62cf2571e23db3665b15a74529c80e976a0d93d6
vantheman <vantheman@thenuoviorizzonticompany.org>
2026-09-15 22:14:13 +0000

    ffi

---
+++ indev/ffi/ffi.h
@@ -0,0 +1,22 @@
+#ifndef FFI_H_
+#define FFI_H_
+
+#include "variables.h"
+
+typedef struct VMFFIvars {
+	VarMap *map;
+	unsigned long count;
+	unsigned long max;
+	unsigned long namesize;
+} VMFFIvars;
+
+void VMFFIallocateVariable
+(VMFFIvars *vars, const char *varname, void *value);
+
+void *VMFFIreadVariableUnsafe
+(VMFFIvars *vars, const char *varname);
+
+void *VMFFIreadVariable /* safer wrapper - never returns NULL, aborts instead */
+(VMFFIvars *vars, const char *varname);
+
+#endif
